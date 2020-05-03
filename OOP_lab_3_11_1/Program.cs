85681dc2700e95e4db434fbbba72321fa3b7cee3
Роман Данилевич <dwordchanel@gmail.com>
2020-05-03 21:31:29 +0300

    Add project files.

---
+++ OOP_lab_3_11_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OOP_lab_3_11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            string[] sentences = str.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int[] lengths = new int[sentences.Length];
+
+            int maxLengthIndex = 0;
+
+            int index = 0;
+
+            foreach (string sentence in sentences)
+            {
+                string[] words = sentence.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                lengths[index] = words.Length;
+
+                ++index;
+            }
+
+            for (int i = 0; i < lengths.Length; ++i)
+            {
+                Console.WriteLine("{0} речення має {1} слiв", i + 1, lengths[i]);
+            }
+
+            for (int i = 0; i < lengths.Length; ++i)
+            {
+                if (lengths[maxLengthIndex] <= lengths[i])
+                {
+                    maxLengthIndex = i;
+                }
+            }
+
+            Console.WriteLine("\nНайдовше речення:\n{0}", sentences[maxLengthIndex]);
+
+        }
+    }
+}
